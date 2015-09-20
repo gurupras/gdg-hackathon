4b1ca65f09b05a50adb0c889f960ac440b6a156d
Guru Prasad Srinivasa <gurupras@buffalo.edu>
2015-09-20 08:15:06 -0400

    Revert "Updated with code from trx repo"
    This reverts commit 832a0d145d074ffe8bcdfd31c9f749139606d672.

--- asound.h
+++ asound.h
@@ -1,14 +1,3 @@
 #define FRAME_SIZE 4
 #define SAMPLE_RATE 44100
-#define NUM_FRAMES 960
-#define BITRATE 128
-#define NUM_CHANNELS 2
-#define BUFFER_TIME 16
-
-#include <alsa/asoundlib.h>
-
-int set_alsa_hw(snd_pcm_t *pcm,
-                unsigned int rate, unsigned int channels,
-                unsigned int buffer);
-int set_alsa_sw(snd_pcm_t *pcm);
-
+#define NUM_FRAMES 32 
